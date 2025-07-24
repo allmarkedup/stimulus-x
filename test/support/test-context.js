6a4eeb7fd971c764cb768635989be958232dd89a
Mark Perkins <mark@allmarkedup.com>
2025-07-25 00:16:48 +0100

    Fix test setup

--- test/support/test-context.js
+++ test/support/test-context.js
@@ -1,5 +1,5 @@
 import { Application } from "@hotwired/stimulus";
-import { StimulusX } from "../../src";
+import StimulusX from "../../src";
 import { nextTick } from "./helpers";
 
 export async function createTestContext() {
